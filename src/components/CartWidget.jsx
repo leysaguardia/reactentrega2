d4ec924aa42b16d3d128df34bb405f84eb447459
leyguardia <leyguardiaj@gmail.com>
2025-05-08 14:49:29 -0400

    vite configuracion

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -11,7 +11,7 @@ const CartWidget = () => {
   const { cantidadTotal } = useContext(CartContext);
 
   return (
-    <Link to="/Cart" className="cartwidget">
+    <Link to="/Cart" className="CartWidget">
       <img width={50} src="src/assets/cart-148964_1280.png" alt="Carrito" />
       <p>Items en el carrito: {cantidadTotal()}</p>
     </Link>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://leysaguardia.github.io/reactentrega2/",
+  base: "/reactentrega2/",
 })
